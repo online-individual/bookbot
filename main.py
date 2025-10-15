3db4e8e250f374c5288b29d0bcf2dd19e1247db5
online-individual <online.individual@protonmail.com>
2025-10-15 16:21:54 -0700

    updated stats.py to count letters // updated main.py to import letter count from stats.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_letters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +9,9 @@ def get_book_text(filepath):
 def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
+    letter_count = count_letters(text)
     total_words = count_words(text)
-    print(f"Found {total_words} total words.")
+    print(f"Found {total_words} total words")
+    print(letter_count)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def count_words(text):
     words = text.split()
     word_count = len(words)
     return word_count
+
+def count_letters(text):
+    convert_to_lowercase = text.lower()
+    counts = {}
+    for ch in convert_to_lowercase:
+        if ch in counts:
+            counts[ch] += 1
+        else:
+            counts[ch] = 1
+    return counts

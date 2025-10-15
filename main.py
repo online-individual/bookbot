76217898ebb7149355f5716a59390fd2f51d7dc4
online-individual <online.individual@protonmail.com>
2025-10-15 15:20:47 -0700

    updated main.py to fetch word count

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
+def count_words(text):
+    words = text.split()
+    word_count = len(words)
+    return word_count
+
 def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
-    print(text)
+    total_words = count_words(text)
+    print(f"Found {total_words} total words.")
 
 main()

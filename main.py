805b7ff0a1dec347b43680e392f1b0470e4c8302
online-individual <online.individual@protonmail.com>
2025-10-15 15:38:09 -0700

    refactored code to produce leaner main.py and new stats.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from stats import count_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
-
-def count_words(text):
-    words = text.split()
-    word_count = len(words)
-    return word_count
 
 def main():
     path = "books/frankenstein.txt"
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(text):
+    words = text.split()
+    word_count = len(words)
+    return word_count
